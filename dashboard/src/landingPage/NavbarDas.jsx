6875250ce92843ebae43db3b4147731ec00f3ce8
GARVIT THAKRAL <garvitthakral1001@gmail.com>
2025-03-17 16:14:21 +0530

    update

--- dashboard/src/landingPage/NavbarDas.jsx
+++ dashboard/src/landingPage/NavbarDas.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const NavbarDas = () => {
+  const [isProDrDo, setIsProDrDo] = useState(false);
+
+  const handleProfile = () => {
+    setIsProDrDo(!isProDrDo);
+  }
+
   return (
     <div className="flex shadow-2xl">
       <div className="w-4/12 pl-13 pr-4 py-4 flex text-sm border-r border-gray-400">
@@ -15,8 +23,68 @@ const NavbarDas = () => {
           <span className="text-gray-500"> -371.83 (-0.60%)</span>
         </p>
       </div>
-      <div className="8/12">
-        <div></div>
+      <div className="w-8/12 px-4 pr-14 flex items-center justify-between">
+        <div>
+          <NavLink to={"/"}>
+            <img src="assets\image\arrow.png" alt="logo" className="h-13" />
+          </NavLink>
+        </div>
+        <div className="flex gap-4">
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive
+                ? "text-logo-s font-semibold"
+                : "text-gray-500 font-light"
+            }
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to={"/orders"}
+            className={({ isActive }) =>
+              isActive
+                ? "text-logo-s font-semibold"
+                : "text-gray-500 font-light"
+            }
+          >
+            Orders
+          </NavLink>
+          <NavLink
+            to={"/holdings"}
+            className={({ isActive }) =>
+              isActive
+                ? "text-logo-s font-semibold"
+                : "text-gray-500 font-light"
+            }
+          >
+            Holdings
+          </NavLink>
+          <NavLink
+            to={"/positions"}
+            className={({ isActive }) =>
+              isActive
+                ? "text-logo-s font-semibold"
+                : "text-gray-500 font-light"
+            }
+          >
+            Positions
+          </NavLink>
+          <NavLink
+            to={"/funds"}
+            className={({ isActive }) =>
+              isActive
+                ? "text-logo-s font-semibold"
+                : "text-gray-500 font-light"
+            }
+          >
+            Funds
+          </NavLink>
+          <div onClick={handleProfile} className={"flex items-center gap-2 cursor-pointer"}>
+            <AccountCircleIcon/>
+            <h3>DemoUser</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
